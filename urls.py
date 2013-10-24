83c10209c4d157c75aeee018b53bcbeaa92e9cb4
Paolo <paul@paolos-air.home>
2013-10-24 01:15:57 +0100

    Custom login page made with bootstrap and crispy forms

--- urls.py
+++ urls.py
@@ -6,8 +6,13 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 # search for dbindexes.py in all INSTALLED_APPS and load them
 dbindexer.autodiscover()
 
-urlpatterns = patterns('',
-        ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-        ('^$', 'django.views.generic.simple.direct_to_template',
-         {'template': 'home.html'}),
-         )
+urlpatterns = patterns(
+    '',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^$', 'django.views.generic.simple.direct_to_template',
+     {'template': 'home.html'}),
+    (r'^accounts/login/$',
+     'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$',
+     'django.contrib.auth.views.logout'),
+)

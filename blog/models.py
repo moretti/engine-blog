6c234172e0f730ca09bc8140a1f8f01c9cd87b85
Paolo <paul@paolos-air.home>
2013-10-24 02:00:38 +0100

    Added a Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    slug = models.SlugField(max_length=150, unique=True)
+    pub_date = models.DateTimeField()
+    author = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.title

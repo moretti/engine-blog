2008d502c877be1182e386c679674134d230d450
Paolo <paul@Paolos-MacBook-Air.local>
2013-10-24 16:25:45 +0100

    About page

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 from blog import views
 
@@ -7,10 +8,12 @@ urlpatterns = patterns(
     '',
     url(r'^$',
         views.PostList.as_view(), name='post_list'),
-    url(r'^new$',
+    url(r'^new-post$',
         login_required(views.PostCreate.as_view()), name='post_new'),
-    url(r'^edit/(?P<pk>\d+)$',
+    url(r'^edit-post/(?P<pk>\d+)$',
         login_required(views.PostUpdate.as_view()), name='post_edit'),
-    url(r'^delete/(?P<pk>\d+)$',
+    url(r'^delete-post/(?P<pk>\d+)$',
         login_required(views.PostDelete.as_view()), name='post_delete'),
+    url(r'^about$',
+        TemplateView.as_view(template_name="about.html")),
 )
--- urls.py
+++ urls.py
@@ -9,11 +9,9 @@ dbindexer.autodiscover()
 urlpatterns = patterns(
     '',
     url('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    url('^$', 'django.views.generic.simple.direct_to_template',
-        {'template': 'home.html'}),
     url(r'^accounts/login/$',
         'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$',
         'django.contrib.auth.views.logout'),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^', include('blog.urls')),
 )
